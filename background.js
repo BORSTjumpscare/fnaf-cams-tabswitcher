51c80c3a10e875cf56892b5c1cd08ca9c28ce9e2
BORSTjumpscare <sepperem@gmail.com>
2026-02-16 16:34:45 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
-// Listen for messages from main.js to switch tabs
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.action === "switchTab") {
     const url = msg.url;
     if (!url) return;
 
-    // Look for existing tab with this URL
     chrome.tabs.query({ url: url }, (tabs) => {
       if (tabs.length > 0) {
         chrome.tabs.update(tabs[0].id, { active: true });
