50ea8b7c5a137b25961c4d5183e298adb9f047f4
BORSTjumpscare <sepperem@gmail.com>
2026-02-16 21:53:04 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,5 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.action === "switchTab") {
-    const url = msg.url;
-    if (!url) return;
-
-    chrome.tabs.query({ url: url }, (tabs) => {
-      if (tabs.length > 0) {
-        chrome.tabs.update(tabs[0].id, { active: true });
-      } else {
-        chrome.tabs.create({ url: url });
-      }
-    });
-  }
+    if (msg.action === "switchTab") {
+        chrome.tabs.update({ url: msg.url });
+    }
 });
